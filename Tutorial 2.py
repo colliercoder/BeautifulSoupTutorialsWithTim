14bd2e818490c3fb292c7fdad55c5e712348ac62
Dan Gauerke <89668769+colliercoder@users.noreply.github.com>
2021-12-03 21:03:09 -0500

    commit

--- Tutorial 2.py
+++ Tutorial 2.py
@@ -27,5 +27,21 @@ print(tags)
 tags = doc.find_all(class_ = "btn-item")
 print(tags)
 
-#regular expression
-tags = doc.find_all(text=)
+#regular expression to find dollar sign
+tags = doc.find_all(text=re.compile("\$.*"))
+for tag in tags:
+    print(tag.strip())
+
+#same as above, just limiting results to 1 result
+tags = doc.find_all(text=re.compile("\$.*"), limit=1)
+for tag in tags:
+    print(tag.strip())
+
+#changing the input tags of type='text' placeholders to be "I have changed you!"
+tags = doc.find_all("input",type="text")
+for tag in tags:
+    tag['placeholder'] = "I have changed you!"
+
+#Writing the changes into a new doc called changed.html
+with open("changed.html",'w') as file:
+    file.write(str(doc))
